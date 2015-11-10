1f600567ccdd873f1932e6110a0fbf9df54a9aee
Ryanz12 <franfebryan@gmail.com>
2015-11-10 18:48:20 +0700

    added ryanz' ref

--- submit.php
+++ submit.php
@@ -1,6 +1,7 @@
 <?php
+	$point;
 	$answers = json_decode(file_get_contents("php://input"));
-	if($answers[0] === "A"){
-		//Jawaban Q1 = A
-	}
+	if($answers[0] === "A"){ $point += 1; }
+	else if($answers[0] === "B"){ $point += 1; }
+	// And so on...
 ?>
