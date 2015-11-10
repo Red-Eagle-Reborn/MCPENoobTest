1b764bb69b0300c7db102c9eb980190b142fe616
DevillordMCPE <maulaxe@yahoo.com>
2015-11-10 18:54:53 +0700

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,7 +1,72 @@
 <?php
-	$point;
 	$answers = json_decode(file_get_contents("php://input"));
-	if($answers[0] === "A"){ $point += 1; }
-	else if($answers[0] === "B"){ $point += 1; }
-	// And so on...
+	$point = 0;
+	$noobie = 1;
+	$pro = 2;
+	if($answers[0] === "A"){
+		$point += $pro;
+	}
+	if($answers[0] === "B"){
+		$point += $noobie;
+	}
+	if($answers[0] === "C"){
+		$point += $noobie;
+	}
+	
+	if($answers[1] === "A"){
+		$point += $noobie;
+	}
+	if($answers[1] === "B"){
+		$point += $pro;
+	}
+	if($answers[1] === "C"){
+		$point += $noobie;
+	}
+	
+	if($answers[2] === "A"){
+		$point += $noobie;
+	}
+	if($answers[2] === "C"){
+		$point += $pro;
+	}
+	
+	if($answers[3] === "A"){
+		$point += $noobie;
+	}
+	if($answers[3] === "B"){
+		$point += $noobie;
+	}
+	if($answers[3] === "C"){
+		$point += $pro;
+	}
+	
+	if($answers[4] === "D"){
+		$point += $pro;
+	}
+	if($answers[4] === "B"){
+		$point += $noobie;
+	}
+	if($answers[4] === "C"){
+		$point += $noobie;
+	}
+	
+	if($answers[5] === "A"){
+		$point += $noobie;
+	}
+	if($answers[5] === "B"){
+		$point += $pro;
+	}
+	if($answers[5] === "C"){
+		$point += $noobie;
+	}
+	
+	if($answers[6] === "A"){
+		$point += $noobie;
+	}
+	if($answers[6] === "B"){
+		$point += $noobie;
+	}
+	if($answers[6] === "C"){
+		$point += $pro;
+	}
 ?>
